68f4ebe158768594722e24c4bfbaf9b2f1c7eea5
YangLock <3167706386@qq.com>
2019-08-23 03:13:48 +0800

    回退了两个版本

--- controllers/api.js
+++ controllers/api.js
@@ -18,7 +18,8 @@
  */
 const APIError = require('../rest').APIError;
 const records = require('../records');
-const multer = require('multer');
+const multer=require('multer');
+var querystring = require('querystring');
 var storage = multer.diskStorage({
   //文件保存路径
   destination: function (req, file, cb) {
@@ -27,108 +28,65 @@ var storage = multer.diskStorage({
   //修改文件名称
   filename: function (req, file, cb) {
     var fileFormat = (file.originalname).split(".");
-    cb(null, Date.now() + "." + fileFormat[fileFormat.length - 1]);
+    cb(null,Date.now() + "." + fileFormat[fileFormat.length - 1]);
   }
 })
 //加载配置
 var upload = multer({ storage: storage });
 
 module.exports = {
-  'POST /upload': function () {
-    upload.single('file'); async (ctx, next) => {
-      ctx.body = {
-        filename: ctx.req.file.filename//返回文件名
-      }
+  'POST /upload': function(){upload.single('file'); async (ctx, next) => {
+    ctx.body = {
+      filename: ctx.req.file.filename//返回文件名
     }
-  },
-  'GET /getuserfindgoods/:user_id': async (ctx, next) => {
+  }},
+  'GET /getuserfindgoods/:user_id': async(ctx, next) => {
     var user_id = ctx.params.user_id;
-    var found = records.getuserfindgoods(user_id);
+    var found = await records.getuserfindgoods(user_id);
     //console.log(`find ${userInfor.length} user:`);
-    // for (let p of found) {
-    //   console.log(JSON.stringify(p));
-    // }
+    for (let p of found) {
+      console.log(JSON.stringify(p));
+    }
     ctx.response.body = found;
   },
-  'GET /getuserfindperson/:user_id': async (ctx, next) => {
+  'GET /getuserfindperson/:user_id': async(ctx, next) => {
     var user_id = ctx.params.user_id;
-    var found = await find_person.findAll({
-      where: {
-        deliver: user_id
-      }
-    });
+    var found = await records.getuserfindperson;
     //console.log(`find ${found.length} user:`);
     for (let p of found) {
       console.log(JSON.stringify(p));
     }
     ctx.response.body = found;
   },
-  'GET /getuserinfor/:user_id': async (ctx, next) => {
+  'GET /getuserinfor/:user_id': async(ctx, next) => {
     var user_id = ctx.params.user_id;
-    var get = records.getuserinfor(user_id);
+    var get=await records.getuserinfor(user_id);
     //console.log(`find ${get.length} user:`);
     for (let p of get) {
       console.log(JSON.stringify(p));
     }
-    console.log(get);
+    //console.log(get);
     ctx.response.body = get;
   },
-  'Get /getmessage/:user_id': async (ctx, next) => {
+  'GET /getmessage/:user_id': async(ctx, next) => {
     var user_id = ctx.params.user_id;
-    var findgood = await find_good.findAll({
-      where: {
-        deliver: user_id
-      }
-    });
-    var findgoodid = new Array(findgood.length);
-    var l = 0;
-    for (l = 0; l < findgood.length; l++) {
-      findgoodid[l] = findgood[l].good_id;
-    }
-    var findperson = await find_person.findAll({
-      where: {
-        deliver: user_id
-      }
-    });
-    var findpersonid = new Array(findperson.length);
-    for (l = 0; l < findperson.length; l++) {
-      findpersonid[l] = findperson[l].good_id;
-    }
-    var good_comment = await good_com.findAll({
-      where: {
-        good_id: findgoodid
-      }
-    });
-    var person_comment = await person_com.findAll({
-      where: {
-        good_id: findpersonid
-      }
-    });
-    var com = new Array(good_comment.length + person_comment.length);
-    var i = 0;
-    for (i = 0; i < good_comment.length; i++) {
-      com[i] = good_comment[i];
-    }
-    for (i = 0; i < person_comment.length; i++) {
-      com[good_comment.length + i] = person_comment[i];
-    }
-    console.log(com);
-    ctx.response.body = com;
-    /*console.log(`find ${userInfor.length} user:`);
-    for (let p of userInfor) {
+    var com=await records.getmessage(user_id);
+    // console.log(`find ${userInfor.length} user:`);
+    for (let p of com) {
         console.log(JSON.stringify(p));
-    }*/
+    }
+    ctx.response.body = com;
   },
-  'DELETE /api/delete/findGood/:id': async (ctx, next) => {
-    console.log(`delete record ${ctx.params.id}...`);
+  'DELETE /api/delete/findGood/:id': async(ctx, next) => {
+    console.log('delete record ${ctx.params.id}...');
     records.deleteRecordFromGood(ctx.params.id);
   },
-  'DELETE /api/delete/findPerson/:id': async (ctx, next) => {
-    console.log(`delete record ${ctx.params.id}...`);
+  'DELETE /api/delete/findPerson/:id': async(ctx, next) => {
+    console.log('delete record ${ctx.params.id}...');
     records.deleteRecordFromPerson(ctx.params.id);
   },
-  'PUT /api/foundConfirm/:id': async (ctx, next) => {
-    console.log(`confirm record ${ctx.params.id}...`);
+  'PUT /api/foundConfirm/:id': async(ctx, next) => {
+    console.log('confirm record ${ctx.params.id}...');
     var record = records.confirmRecordFromGood(ctx.params.id);
     if (record) {
       ctx.rest(record);
@@ -136,8 +94,8 @@ module.exports = {
       throw new APIError('record:not_found', 'record not found by id');
     }
   },
-  'PUT /api/claimConfirm/:id': async (ctx, next) => {
-    console.log(`confirm record ${ctx.params.id}...`);
+  'PUT /api/claimConfirm/:id': async(ctx, next) => {
+    console.log('confirm record ${ctx.params.id}...');
     var record = records.confirmRecordFromPerson(ctx.params.id);
     if (record) {
       ctx.rest(record);
@@ -145,8 +103,8 @@ module.exports = {
       throw new APIError('record:not_found', 'record not found by id');
     }
   },
-  'PUT /api/reEdit/findGood/:id': async (ctx, next) => {
-    console.log(`reedit record ${ctx.params.id}...`);
+  'PUT /api/reEdit/findGood/:id': async(ctx, next) => {
+    console.log('reedit record ${ctx.params.id}...');
     var record = records.reeditRecordFromGood(ctx.params.id);
     if (record[0] && record[1]) {
       ctx.rest(record);
@@ -154,8 +112,8 @@ module.exports = {
       throw new APIError('record:not_found', 'record not found by id');
     }
   },
-  'PUT /api/reEdit/findPerson/:id': async (ctx, next) => {
-    console.log(`reedit record ${ctx.params.id}...`);
+  'PUT /api/reEdit/findPerson/:id': async(ctx, next) => {
+    console.log('reedit record ${ctx.params.id}...');
     var record = records.reeditRecordFromPerson(ctx.params.id);
     if (record[0] && record[1]) {
       ctx.rest(record);
@@ -163,8 +121,8 @@ module.exports = {
       throw new APIError('record:not_found', 'record not found by id');
     }
   },
-  'PUT /api/editMyInfo/:id': async (ctx, next) => {
-    console.log(`edit user ${ctx.params.id} own information`);
+  'PUT /api/editMyInfo/:id': async(ctx, next) => {
+    console.log('edit user ${ctx.params.id} own information');
     var record = records.editUserInfo(ctx.params.id);
     if (record) {
       ctx.rest(record);
@@ -172,8 +130,8 @@ module.exports = {
       throw new APIError('record:not_found', 'record not found by id');
     }
   },
-  'PUT /api/refresh/findGood/:id': async (ctx, next) => {
-    console.log(`refresh record ${ctx.params.id}...`);
+  'PUT /api/refresh/findGood/:id': async(ctx, next) => {
+    console.log('refresh record ${ctx.params.id}...');
     var record = records.refreshFromGood(ctx.params.id);
     if (record) {
       ctx.rest(record);
@@ -181,8 +139,8 @@ module.exports = {
       throw new APIError('record:not_found', 'record not found by id');
     }
   },
-  'PUT /api/refresh/findPerson/:id': async (ctx, next) => {
-    console.log(`refresh record ${ctx.params.id}...`);
+  'PUT /api/refresh/findPerson/:id': async(ctx, next) => {
+    console.log('refresh record ${ctx.params.id}...');
     var record = records.reeditRecordFromPerson(ctx.params.id);
     if (record) {
       ctx.rest(record);
@@ -190,45 +148,47 @@ module.exports = {
       throw new APIError('record:not_found', 'record not found by id');
     }
   },
- 'POST /api/release/findPerson': async (ctx, next) => {
-console.log('release a good（pick up）');
-records.releasePersons();
-// 检验机制先不写
-},
-'POST /api/release/findGood': async (ctx, next) => {
-console.log('release a good(lost)');
-records.releaseGoods();
-// 检验机制先不写
-},
-'POST /api/release/goodCom': async (ctx, next) => {
-console.log('release a message g');
-records.releaseGoodmessages();
-// 检验机制先不写
-},
-'POST /api/release/personCom': async (ctx, next) => {
-console.log('release a message p');
-records.releasePersonmessages();
-// 检验机制先不写
-},
-'GET /api/get/findGood/:kind': async (ctx, next) => {
-var kind = ctx.params.kind;
-var result = records.getfindgoods(kind);
-ctx.response.body = result;
-},
-'GET /api/get/findPerson/:kind': async (ctx, next) => {
-var kind = ctx.params.kind;
-var result = records.getfindpersons(kind);
-ctx.response.body = result;
-},
-'GET /api/get/searchGood/:keyword': async (ctx, next) => {
-var keyword = ctx.params.keyword;
-var result = records.getsearchgoods(keyword);
-ctx.response.body = result;
-},
-'GET /api/get/searchPerson/:keyword': async (ctx, next) => {
-var keyword = ctx.params.keyword;
-var result = records.getsearchpersons(keyword);
-ctx.response.body = result;
-}
-
+  'POST /api/release/findPerson': async(ctx, next) => {
+    console.log('release a good（pick up）');
+    records.releasePersons();
+    // 检验机制先不写
+  },
+  'POST /api/release/findGood': async(ctx, next) => {
+    console.log('release a good(lost)');
+    records.releaseGoods();
+    // 检验机制先不写
+  },
+  'POST /api/release/goodCom': async(ctx, next) => {
+    console.log('release a message');
+    records.releasemessages();
+    // 检验机制先不写
+  },
+  'GET /api/get/findGood/:kind': async(ctx, next) => {
+    var kind = ctx.params.kind;
+    var result = await records.getfindgoods(kind);
+    ctx.response.body = result;
+  },
+  'GET /api/get/findPerson/:kind': async(ctx, next) => {
+    var kind = ctx.params.kind;
+    var result = await records.getfindpersons(kind);
+    ctx.response.body = result;
+  },
+  'GET /api/get/searchGood/:keyword': async(ctx, next) => {
+    var keyword = ctx.params.keyword;
+    var result = records.getsearchgoods(keyword);
+    ctx.response.body = result;
+  },
+  'GET /api/get/searchPerson/:keyword': async(ctx, next) => {
+    var keyword = ctx.params.keyword;
+    var result = records.getsearchpersons(keyword);
+    ctx.response.body = result;
+  },
+  'GET /api/get/checkuser':async(ctx,next)=>{
+    var params = querystring.parse(ctx.req._parsedUrl.query);
+    var user_id=params.user_id;
+    var user_name=params.user_name;
+    var user_avatar=params.user_avatar;
+    records.checkuser(user_id,user_name,user_avatar);
+    ctx.response.body=('checked');
+  }
 };
